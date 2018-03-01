df448ea0e89e5e0825aa9a8461a5ea9cf57b2c6a
jeremyy55 <jeremy.DUMONT@student.umons.ac.be>
2018-03-01 22:08:47 +0100

    mother fucker

--- ride.py
+++ ride.py
@@ -15,11 +15,12 @@ def feasable(car, ride):
 		return True
 	else:
 		return False
-#def goForRide(cars,rides,car2modif,ride2delete):
-#	move1=int( deplacement([int(car[0]),int(car[1])],[int(ride[0]),int(ride[1])]))
-#	move2=int( deplacement([int(ride[0]),int(ride[1])],[int(ride[2]),int(ride[3])]))
-#	cars[car2modif]=cars[2]+ move1 + move2
-#	ride
+def goForRide(cars,rides,car2modif,ride2delete):
+	move1=int( deplacement([int(car2modif[0]),int(car2modif[1])],[int(ride2delete[0]),int(ride2delete[1])]))
+	move2=int( deplacement([int(ride2delete[0]),int(ride2delete[1])],[int(ride2delete[2]),int(ride2delete[3])]))
+	cars[car2modif]=cars[car2modif]+ move1 + move2
+	rides.remove(ride2delete)
+	return cars, rides
 
 
 def main():
