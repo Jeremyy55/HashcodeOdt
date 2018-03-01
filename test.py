bda7ead9922d5795f193ccfe8a92acde6c08ea49
Gauthier Deroux <gauthier.deroux1@gmail.com>
2018-03-01 19:51:05 +0100

    Test update

--- test.py
+++ test.py
@@ -1,8 +1,16 @@
 from handleFile import *
 
+
+def calculGauth(tab):
+	for line in tab:
+		if abs(line[2]-line[0])+abs(line[3]-line[1])<line[5]:
+			print("ok")
+		else:
+			print("Dommage")
 def main():
 	damn = text2lines("data/a_example.in")
-	print(damn)
+	print(len(damn))
+	calculGauth(damn)
 
 if __name__ == '__main__':
 	main()
