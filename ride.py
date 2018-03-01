9805abc45d87eea8b2feb0809656d51dbb52e767
jeremyy55 <jeremy.DUMONT@student.umons.ac.be>
2018-03-01 21:49:09 +0100

    feasable ok

--- ride.py
+++ ride.py
@@ -1,33 +1,30 @@
 
 from handleFile import *
+from forCar import *
 
 def init(tab):
 	car=[]
 	for i in range(int(tab)) :
 		car.append([0,0,0])
 	return car
-def feasable(tab):
-	for line in tab:
-		if abs(int(line[2])-int(line[0]))+abs(int(line[3])-int(line[1]))<int(line[5]):
-			return True
-		else:
-			return False
+def feasable(car, ride):
+	move1=int( deplacement([int(car[0]),int(car[1])],[int(ride[0]),int(ride[1])]))
+	move2=int( deplacement([int(ride[0]),int(ride[1])],[int(ride[2]),int(ride[3])]))
+	if(car[2]+move1+move2)<int(ride[5]):
+			#+<
 
-def deplacement(tab):
-	if feasable(tab):
-		print("ok")
+		return True
+	else:
+		return False
+
+#def deplacement(tab):
+#	if feasable(tab):
+#		print("ok")
 def main():
 	damn = text2lines("data/a_example.in")
-	print(len(damn))
-
 	[R, C, F, N, B, T], already = extract1line(damn)
-	print('test test')
-	print(R)
-	print(C)
-	print(already)
 	car = init(F)
-	print(car)
-	#deplacement(damn)
+	print(feasable(car[0],already[0]))
 
 if __name__ == '__main__':
 	main()
